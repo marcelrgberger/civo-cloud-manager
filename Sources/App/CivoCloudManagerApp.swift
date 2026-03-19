27bd0ef0e8c1cf9c07e18a902e86e247cf23482a
Marcel R. G. Berger <hello@marcelrgberger.com>
2026-03-19 22:04:07 +0200

    Fix menu bar icon not appearing: remove setActivationPolicy calls

--- Sources/App/CivoCloudManagerApp.swift
+++ Sources/App/CivoCloudManagerApp.swift
@@ -3,7 +3,6 @@ import SwiftUI
 @main
 struct CivoCloudManagerApp: App {
     @State private var appState = AppState()
-    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     @Environment(\.openWindow) private var openWindow
 
     init() {
@@ -21,40 +20,14 @@ struct CivoCloudManagerApp: App {
 
         Window("Civo Cloud Manager Setup", id: "onboarding") {
             OnboardingView(state: appState)
-                .onDisappear {
-                    demoteIfNoWindows()
-                }
         }
         .windowResizability(.contentSize)
         .defaultPosition(.center)
 
         Window("Civo Cloud Manager", id: "main") {
             MainWindowView()
-                .onDisappear {
-                    demoteIfNoWindows()
-                }
         }
         .defaultSize(width: 1100, height: 700)
         .defaultPosition(.center)
-    }
-
-    private func demoteIfNoWindows() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-            let hasVisibleWindow = NSApp.windows.contains { $0.isVisible && $0.level == .normal }
-            if !hasVisibleWindow {
-                NSApp.setActivationPolicy(.accessory)
-            }
-        }
-    }
-}
-
-class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            let hasVisibleWindow = NSApp.windows.contains { $0.isVisible && $0.level == .normal }
-            if !hasVisibleWindow {
-                NSApp.setActivationPolicy(.accessory)
-            }
-        }
     }
 }
